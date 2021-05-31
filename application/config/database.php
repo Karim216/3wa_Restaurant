a10bd4c8db352c4e8331c22b6fb4dbe13be52852
karim216 <ahmedkarim.njiawouogbetnkom@esprit.tn>
2021-05-31 18:36:29 +0100

    first commit

---
+++ application/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Database configuration settings used by PDO.
+ */
+
+$config['dsn']      = 'mysql:host=localhost;dbname=resto;charset=UTF8';
+$config['password'] = 'root';
+$config['user']     = 'root';
